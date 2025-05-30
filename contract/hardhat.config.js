1710b61df7131084e798321c50f722512939fa7f
rtree <arakitomohiko@gmail.com>
2025-05-30 22:12:50 +0200

    ong

--- contract/hardhat.config.js
+++ contract/hardhat.config.js
@@ -1,4 +1,13 @@
-/** @type import('hardhat/config').HardhatUserConfig */
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
 module.exports = {
-  solidity: "0.8.28",
+  networks: {
+    worldchain: {
+      url: "https://worldchain-mainnet.g.alchemy.com/v2/yYqkQNEKuzDKgYc35KTC35iwZ9oHRy3u",
+      chainId: 480,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+  solidity: "0.8.19",
 };
