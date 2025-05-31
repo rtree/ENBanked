1beddcffb3dd19b01dea2f8bc38f3e181d72950d
rtree <arakitomohiko@gmail.com>
2025-06-01 00:51:57 +0200

    ong

--- product/frontend/src/config.ts
+++ product/frontend/src/config.ts
@@ -1,7 +1,7 @@
 // PRJROOT/frontend/src/config.ts
 // すべてのファイルで import して使う
 export const APP_ID          = 'app_c22b23e8101db637591586c4a8ca02b1';
-export const VAULT_ADDRESS   = '0xC89341BDAAcD79Ad641a79FC671511f0E7192E48'; // VaultZkWei
+export const VAULT_ADDRESS   = '0x333546C6B14818B4389e92d3e1574396104EA09e'; // VaultZkWei
 export const TREE_DEPTH      = 3;         // Vault と合わせる
 export const AMOUNT_HEX      = '0x1';     // 1 wei
 export const RPC_URL = 'https://worldchain-mainnet.g.alchemy.com/v2/yYqkQNEKuzDKgYc35KTC35iwZ9oHRy3u';
