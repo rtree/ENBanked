c37369917e3c5eea339c4f38c20448de3dac90c4
rtree <arakitomohiko@gmail.com>
2025-06-01 00:39:00 +0200

    ong

--- product/contract/circuits/withdraw.circom
+++ product/contract/circuits/withdraw.circom
@@ -25,7 +25,7 @@ template Withdraw() {
         h[i].inputs[1] <== IfElse()(pathIndices[i], cur, pathElements[i]);
         cur = h[i].out;
     }
-    //cur === root;
+    cur === root;
 
     component nH = Poseidon(1);
     nH.inputs[0] <== n;
--- product/frontend/src/config.ts
+++ product/frontend/src/config.ts
@@ -1,7 +1,7 @@
 // PRJROOT/frontend/src/config.ts
 // すべてのファイルで import して使う
 export const APP_ID          = 'app_c22b23e8101db637591586c4a8ca02b1';
-export const VAULT_ADDRESS   = '0xA1ea22EF9E40339B18f187E54D82b29C2628f568'; // VaultZkWei
+export const VAULT_ADDRESS   = '0xC89341BDAAcD79Ad641a79FC671511f0E7192E48'; // VaultZkWei
 export const TREE_DEPTH      = 3;         // Vault と合わせる
 export const AMOUNT_HEX      = '0x1';     // 1 wei
 export const RPC_URL = 'https://worldchain-mainnet.g.alchemy.com/v2/yYqkQNEKuzDKgYc35KTC35iwZ9oHRy3u';
