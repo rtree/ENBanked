e521cee692797c41572a9511653ea73f3fc32aa5
rtree <arakitomohiko@gmail.com>
2025-05-31 14:51:52 +0200

    ong

--- product/frontend/src/config.ts
+++ product/frontend/src/config.ts
@@ -1,5 +1,5 @@
 // すべてのファイルで import して使う
 export const APP_ID          = 'app_c22b23e8101db637591586c4a8ca02b1';
-export const VAULT_ADDRESS   = '0xA55b1bBa54B2d9F31f6B5a83BA2eDAc5320D0a22'; // VaultZkWei
+export const VAULT_ADDRESS   = '0x70a6EEccEAB2F2e2ffBD20b960f4cA205675ba1c'; // VaultZkWei
 export const TREE_DEPTH      = 3;         // Vault と合わせる
 export const AMOUNT_HEX      = '0x1';     // 1 wei
