16dcd63b06dff6e20141417791319dceb15b19b5
rtree <arakitomohiko@gmail.com>
2025-06-01 06:59:20 +0200

    og

--- product/frontend/src/config.ts
+++ product/frontend/src/config.ts
@@ -1,7 +1,7 @@
 // PRJROOT/frontend/src/config.ts
 // すべてのファイルで import して使う
 export const APP_ID          = 'app_c22b23e8101db637591586c4a8ca02b1';
-export const VAULT_ADDRESS   = '0xA2cd8a8fB39454c341EC69f195eDC6D976db8f10'; // VaultZkWei
+export const VAULT_ADDRESS   = '0xDC9D04B5BB83C30b606f42FdA7518D46Fe177fb0'; // VaultZkWei
 export const MOCK_VAULT_ADDRESS = '0x4934C573FA9a8B72EA0325e20CfA4d72365045C2'
 export const TREE_DEPTH      = 3;         // Vault と合わせる
 export const AMOUNT_HEX      = '0x1';     // 1 wei
